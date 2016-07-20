e3aa40553ef1caca485e680026c8984ce77570a0
Alec Hipshear <alechipshear@gmail.com>
2016-07-20 19:19:31 -0400

    Simplify rails template even further

--- rails_template.rb
+++ rails_template.rb
@@ -1,36 +1,14 @@
-def commit(msg)
-  git add: "."
-  git commit: "-m '#{msg}'"
-end
-
-git :init
-git add: '.'
-git commit: "-a -m 'Initial commit'"
-
 file 'Procfile', "web: bundle exec rails server -p $PORT"
 file '.env', "RAILS_ENV=development\nPORT=3000\n"
-
-commit "Setup foreman"
-
-gem_group :development do
-  gem 'guard'
-  gem 'guard-rspec', require: false
-  gem 'ruby-prof'
-end
 
 gem_group :development, :test do
   gem 'factory_girl_rails'
   gem 'rspec-rails'
   gem 'faker'
-  unless options["skip_sprockets"] || options["api"]
-    gem 'quiet_assets'
-  end
 end
 
 after_bundle do
   generate "rspec:install", "--force"
-  run("guard init")
-  run("guard init rspec")
   application do
     <<-CODE
 
@@ -42,5 +20,8 @@ after_bundle do
     end
     CODE
   end
-  commit "Finish setup"
+
+  git :init
+  git add: '.'
+  git commit: "-a -m 'Initial commit'"
 end
